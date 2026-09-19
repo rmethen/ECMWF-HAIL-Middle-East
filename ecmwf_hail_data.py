12f1ebf65a0e8af206f4d200a5bcc5ea178bf6a2
rmethen <rashed99601018@gmail.com>
2026-09-19 21:05:58 +0300

    Fix indentation for surface_target assignment

--- ecmwf_hail_data.py
+++ ecmwf_hail_data.py
@@ -67,7 +67,7 @@ def download_ecmwf_hail_fields():
         target=str(target),
     )
     
-  surface_target = Path("data/ecmwf_surface_wind_0_72h.grib2")
+    surface_target = Path("data/ecmwf_surface_wind_0_72h.grib2")
     print("Downloading ECMWF 10-m surface wind...")
     client.retrieve(
         type="fc",
