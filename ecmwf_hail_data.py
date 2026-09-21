76d572501378846fe4cd8582b5c61909bff4ac28
rmethen <rashed99601018@gmail.com>
2026-09-21 22:59:39 +0300

    Fix SURFACE_PARAMS list by removing 'fg10'

--- ecmwf_hail_data.py
+++ ecmwf_hail_data.py
@@ -7,7 +7,7 @@ NORTH, WEST, SOUTH, EAST = 45, 20, 10, 65
 STEPS = list(range(0, 73, 3))
 LEVELS = [1000, 925, 850, 700, 600, 500, 400, 300, 200]
 PRESSURE_PARAMS = ["t", "u", "v", "q", "w", "gh"]
-SURFACE_PARAMS = ["2t", "2d", "10u", "10v", "fg10", "msl", "tp"]
+SURFACE_PARAMS = ["2t", "2d", "10u", "10v", "msl", "tp"]
 
 
 def download_ecmwf_hail_fields():
